cc529e1463d3fbcd8dbf2f1fdfb84013d30de114
Jens Berg <jensberg@live.se>
2021-03-17 22:55:04 +0100

    Add Send overload for NetMessage in UDPSocket

--- Source/Network/Socket/UDPSocket.cpp
+++ Source/Network/Socket/UDPSocket.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "UDPSocket.h"
 #include "Address/Address.h"
+#include "NetMessage.h"
 #include <iostream>
 
 Network::UDPSocket::UDPSocket()
@@ -15,6 +16,14 @@ Network::UDPSocket::UDPSocket()
 int Network::UDPSocket::Send(const char* aDataBuf, int aBufLen, const Address& aAddress)
 {
 	return sendto(mySocket, aDataBuf, aBufLen, 0, (sockaddr*)&aAddress, sizeof(aAddress));
+}
+
+int Network::UDPSocket::Send(const NetMessage& aMessage, const Address& aAddress)
+{
+	char buf[Constants::MAX_BUFFER_SIZE];
+	buf[0] = unsigned char(1); // Message count
+	memcpy(buf + 1, &aMessage.myMessageID, aMessage.mySize);
+	return Send(buf, aMessage.mySize, aAddress);
 }
 
 bool Network::UDPSocket::Receive(const char* aDataBuf, int aBufLen, const Address& aAddress, int& aReceivedLength)
--- Source/Network/Socket/UDPSocket.h
+++ Source/Network/Socket/UDPSocket.h
@@ -2,11 +2,13 @@
 #include "Socket.h"
 namespace Network
 {
+	class NetMessage;
 	class UDPSocket : public Socket
 	{
 	public:
 		UDPSocket();
 		int Send(const char* aDataBuf, int aBufLen, const Address& aAddress);
+		int Send(const NetMessage& aMessage, const Address& aAddress);
 		bool Receive(const char* aDataBuf, int aBufLen, const Address& aAddress, int& aReceivedLength);
 	};
 }
